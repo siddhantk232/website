dc27b4004aa86939c29721027fc94c573e7668b5
siddhantk232 <siddhantk232@gmail.com>
2020-08-13 16:34:46 +0530

    feat: eleventy navigation setup change: parse md with njk

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,12 @@
+const pluginNavigation = require("@11ty/eleventy-navigation");
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("assets");
   eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPlugin(pluginNavigation);
+
+  return {
+    markdownTemplateEngine: "njk"
+  };
 };
