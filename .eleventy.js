9dac795bf5577958e8f1219ab97c84eae6629c94
siddhantk232 <siddhantk232@gmail.com>
2020-08-16 12:46:24 +0530

    feat: syntax highlight in blogs

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,12 @@
 const pluginNavigation = require("@11ty/eleventy-navigation");
 const lazyImagesPlugin = require("eleventy-plugin-lazyimages");
 const sitemap = require("@quasibit/eleventy-plugin-sitemap");
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("assets");
   eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPlugin(syntaxHighlight);
   eleventyConfig.addPlugin(lazyImagesPlugin, {
     imgSelector: ".post-content img",
     scriptSrc: "/assets/lazysizes.min.js"
