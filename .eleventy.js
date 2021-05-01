66a8aed2250b843640a691a0f3d9a3c65d962ba8
siddhantk232 <siddhantk232@gmail.com>
2021-05-01 18:27:29 +0530

    feat: rss feed and other small changes

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ const lazyImagesPlugin = require("eleventy-plugin-lazyimages");
 const sitemap = require("@quasibit/eleventy-plugin-sitemap");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const readingTime = require("eleventy-plugin-reading-time");
+const pluginRss = require("@11ty/eleventy-plugin-rss");
 
 const { DateTime } = require("luxon");
 
@@ -12,16 +13,17 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(syntaxHighlight);
   eleventyConfig.addPlugin(lazyImagesPlugin, {
     imgSelector: ".post-content img",
-    scriptSrc: "/assets/lazysizes.min.js"
+    scriptSrc: "/assets/lazysizes.min.js",
   });
   eleventyConfig.addPlugin(sitemap, {
     sitemap: {
-      hostname: "https://www.siddhant.codes"
-    }
+      hostname: "https://www.siddhant.codes",
+    },
   });
   eleventyConfig.addPlugin(readingTime);
+  eleventyConfig.addPlugin(pluginRss);
 
-  eleventyConfig.addFilter("readableDate", dateObj => {
+  eleventyConfig.addFilter("readableDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat(
       "dd LLLL yyyy"
     );
@@ -30,7 +32,7 @@ module.exports = function (eleventyConfig) {
   return {
     markdownTemplateEngine: "njk",
     dir: {
-      input: "src"
-    }
+      input: "src",
+    },
   };
 };
