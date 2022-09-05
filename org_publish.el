cef422ecb3c554c83a56cd6cfb194344b1e185fe
siddhantCodes <siddhantk232@gmail.com>
2022-09-05 22:31:09 +0530

    notes: org publish script

--- org_publish.el
+++ org_publish.el
@@ -3,15 +3,16 @@
 ;; Define the publishing project
 (setq org-publish-project-alist
       (list
-       ;; (list "studies"
-       ;;       :recursive t
-       ;;       :base-directory "/home/sidd/vimwiki/studies/"
-       ;;       :publishing-directory "./src/notes"
-       ;;       :publishing-function 'org-html-publish-to-html)
+       (list "studies"
+             :makeindex t
+             :recursive t
+             :base-directory "/home/sidd/vimwiki/studies/"
+             :publishing-directory "./src/notes"
+             :publishing-function 'org-html-publish-to-html)
        (list "static"
         :recursive t
-        :base-directory "/homr/sidd/vimwiki/studies/"
-        :base-extension "c\\|cc\\|css\\|js\\|png\\|jpg\\|jpeg\\|svg\\|gif\\|pdf\\|mp3\\|swf\\|zip\\|gz\\|txt\\|el\\|tar.gz\\|c\\|cpp\\|sh"
+        :base-directory "/home/sidd/vimwiki/studies/"
+        :base-extension "jpg\\|gif\\|png"
         :publishing-directory "./src/notes"
         :publishing-function 'org-publish-attachment)))
 
