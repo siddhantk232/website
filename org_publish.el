79887fa8eceea9ea155e2322af8c54beea9977d2
siddhantCodes <siddhantk232@gmail.com>
2022-09-05 22:48:26 +0530

    fix: htmlize package
    code blocks in notes are colorised now!

--- org_publish.el
+++ org_publish.el
@@ -1,5 +1,19 @@
+;; Set the package installation directory so that packages aren't stored in the
+;; ~/.emacs.d/elpa path.
+(require 'package)
+(setq package-user-dir (expand-file-name "./.packages"))
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+
+;; Initialize the package system
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; Install dependencies
+(package-install 'htmlize)
+
 (require 'ox-publish)
-(require 'htmlize)
 
 ;; Define the publishing project
 (setq org-publish-project-alist
@@ -18,3 +32,5 @@
         :publishing-function 'org-publish-attachment)))
 
 (org-publish-all t)
+
+(message "build finished!")
