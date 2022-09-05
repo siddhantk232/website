f889d57403465b2d69a3f5f054dd4282f208b6e0
siddhantCodes <siddhantk232@gmail.com>
2022-09-05 22:23:57 +0530

    updated notes:
    org_publish.sh
    src/notes/cpp_quickstart.html
    src/notes/cpp_stl.html
    src/notes/dsa_in_c.html
    src/notes/cp/
    src/notes/dsa/
    src/notes/index.html
    src/notes/numerical_methods/
    src/notes/oop_cpp/

--- org_publish.sh
+++ org_publish.sh
@@ -3,3 +3,16 @@
 emacs -Q --script org_publish.el
 
 mv src/notes/theindex.html src/notes/index.html
+
+changed_files=`git status --short | awk '{print $2}'`
+
+read -r -d '' commit_message << EOM
+updated notes:
+
+${changed_files}
+EOM
+
+
+git add .
+git commit -m "$commit_message"
+git push
