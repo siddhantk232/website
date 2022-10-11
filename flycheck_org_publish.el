c0ee1f1f89d9934a9a3a4e099cc914b4fa77d7c6
siddhantCodes <siddhantk232@gmail.com>
2022-10-11 18:37:06 +0530

    updated notes:
    src/notes/cp/bits.html
    src/notes/cp/cpp_quickstart.html
    src/notes/cp/cpp_stl.html
    src/notes/cp/temp.html
    src/notes/dsa/c_quickstart.html
    src/notes/dsa/dsa_in_c.html
    src/notes/dsa/ppt.html
    src/notes/dsa/prac1.html
    src/notes/dsa/prac2.html
    src/notes/dsa/prac3.html
    src/notes/index.html
    src/notes/numerical_methods/notes.html
    src/notes/oop_cpp/oop_cpp.html
    src/notes/oop_cpp/prac1.html
    src/notes/oop_cpp/prac2.html
    src/notes/oop_cpp/prac3.html
    src/notes/oop_cpp/prac4.html
    src/notes/oop_cpp/prac5.html
    src/notes/oop_cpp/practicals.html
    flycheck_org_publish.el

---
+++ flycheck_org_publish.el
@@ -0,0 +1,39 @@
+;; Set the package installation directory so that packages aren't stored in the
+;; ~/.emacs.d/elpa path.
+(require 'package)
+(setq package-user-dir (expand-file-name "./.packages"))
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+
+;; Initialize the package system
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+(require 'ox-publish)
+
+;; Install dependencies
+(package-install 'htmlize)
+
+(setq org-html-htmlize-output-type 'css)
+
+;; Define the publishing project
+(setq org-publish-project-alist
+      (list
+       (list "studies"
+             :makeindex t
+             :recursive t
+             :base-directory "/home/sidd/vimwiki/studies/"
+             :publishing-directory "./src/notes"
+             :html-html5-fancy t
+             :publishing-function 'org-html-publish-to-html)
+       (list "static"
+        :recursive t
+        :base-directory "/home/sidd/vimwiki/studies/"
+        :base-extension "jpg\\|gif\\|png"
+        :publishing-directory "./src/notes"
+        :publishing-function 'org-publish-attachment)))
+
+(org-publish-all t)
+
+(message "build finished!")
